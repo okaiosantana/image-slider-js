e3783c05eae0c4262d5c44fbd020da72444dc032
Kaio Santana <contato.kaiosantana@gmail.com>
2026-02-11 10:46:37 -0300

    feat(slider): adiciona debounce para otimizar o redimensionamento

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+import debounce from "./debounce.js";
+
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
@@ -124,7 +126,8 @@ export default class Slide {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
-    this.onResize = this.onResize.bind(this);
+
+    this.onResize = debounce(this.onResize.bind(this), 200);
   }
 
   init() {
